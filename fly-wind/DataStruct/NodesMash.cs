2326cba81e4856440197040eb403b664e9b8fa50
zy <zeng.yu.1993@gmail.com>
2017-03-15 17:20:47 +0800

    + 设置 NodesMash 的接口。

--- fly-wind/DataStruct/NodesMash.cs
+++ fly-wind/DataStruct/NodesMash.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace fly_wind.DataStruct {
     class NodesMoah {
+
+        private HashSet<MashNode> nodes = new HashSet<MashNode>();
+
+        public HashSet<MashNode> getMash() {
+            return nodes;
+        }
     }
 }

4846a07c0a49ad5daf6e32d3d439f9b3e5b7c22d
zy <zeng.yu.1993@gmail.com>
2017-03-15 20:23:15 +0800

    + 两个坐标接口。

---
+++ fly-wind/DataStructs/Point/Vector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fly_wind.DataStructs.Point {
+    interface IVector3 {
+        double x { get; set; }
+        double y { get; set; }
+        double z { get; set; }
+        double distanceFrom(double x, double y, double z);
+        double distanceFrom(IVector3 vec3);
+    }
+
+    interface IVector2 {
+        double x { get; set; }
+        double y { get; set; }
+        double distanceFrom(double x, double y);
+        double distanceFrom(IVector2 vec2);
+    }
+}

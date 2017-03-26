2cac6a4b1631d648217b3621427063e244ae64bc
zy <zeng.yu.1993@gmail.com>
2017-03-26 18:04:05 +0800

    + 用简单的方法实现和邻接点的查找和图内路径搜索。同时编写了单元测试。

---
+++ GroundController/Core.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundController {
+
+    public class Vertex {
+
+        public double x;
+        public double y;
+        public double z;
+
+        private List<Vertex> contracts = new List<Vertex>();
+
+
+        public Vertex(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public List<Vertex> GetContracts() {
+            return contracts;
+        }
+
+        public int AddContract(Vertex vertex) {
+            contracts.Add(vertex);
+            return contracts.Count;
+        }
+    }
+
+    public class Graph {
+
+        private List<Vertex> vertexs = new List<Vertex>();
+
+        public Graph() { }
+
+        public int Add(Vertex v) {
+            vertexs.Add(v);
+            return vertexs.Count;
+        }
+
+        public List<Vertex> GetVertexs() {
+            return vertexs;
+        }
+    }
+
+    public class Core {
+
+        /// <summary>
+        /// 给出一个点，在它的邻接点集合中找出一个最靠近目标的点。
+        /// </summary>
+        /// <param name="current">指定点</param>
+        /// <param name="destination">目标点</param>
+        /// <returns>最符合要求的点，可以是指定点自身</returns>
+        public Vertex NextVertex(Vertex current, Vertex destination) {
+            List<Vertex> contracts = current.GetContracts();
+            Vertex bestMatch = current;
+
+            double currentDistance = Math.Sqrt(
+                Math.Pow(destination.x - current.x, 2f) +
+                Math.Pow(destination.y - current.y, 2f) +
+                Math.Pow(destination.z - current.z, 2f));
+
+            foreach (Vertex ver in contracts) {
+                double nextDistance = Math.Sqrt(
+                    Math.Pow(destination.x - ver.x, 2f) +
+                    Math.Pow(destination.y - ver.y, 2f) +
+                    Math.Pow(destination.z - ver.z, 2f));
+                if (nextDistance < currentDistance) {
+                    bestMatch = ver;
+                } 
+            }
+
+            return bestMatch;
+        }
+
+        /// <summary>
+        /// 在给出的图中求出一条从起点到终点的点序列，序列中包含起点但不含终点。
+        /// </summary>
+        /// <param name="net">图</param>
+        /// <param name="start">起点，必须在图中</param>
+        /// <param name="end">终点</param>
+        /// <returns>点序列，如果起点不在图中则返回null，否则返回一个不为空的序列</returns>
+        public List<Vertex> GenerateWayPointSecquence(Graph net, Vertex start, Vertex end) {
+            List<Vertex> wayPoints = new List<Vertex>();
+            List<Vertex> vertexs = net.GetVertexs();
+
+            if (null == vertexs.Find(x => x.GetHashCode() == start.GetHashCode())) {
+                throw new ArgumentException("start point not in net");
+            }
+
+            Vertex current = start;
+            while (current != end) {
+                wayPoints.Add(current);
+                Vertex next = NextVertex(current, end);
+                if (current == next) {
+                    break;
+                }
+                current = next;
+            }
+
+            return wayPoints;
+        }
+
+    }
+}
---
+++ GroundControllerTests/CoreTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroundController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundController.Tests {
+    [TestClass()]
+    public class CoreTests {
+        [TestMethod()]
+        public void nextVertexTest() {
+            Vertex start = new Vertex(0f, 0f, 0f);
+            Vertex end = new Vertex(10f, 10f, 10f);
+
+            start.AddContract(new Vertex(-1f, -1f, -1f));
+            start.AddContract(new Vertex(1f, 1f, 1f));
+
+            Vertex bestMatch = new Vertex(2f, 2f, 2f);
+            start.AddContract(bestMatch);
+
+            Core core = new Core();
+
+            Assert.AreEqual(bestMatch, core.NextVertex(start, end));
+        }
+
+        [TestMethod()]
+        public void GenerateWayPointSecquenceTest() {
+            Graph net = new Graph();
+            List<Vertex> vertexs = new List<Vertex>();
+
+            Vertex[,,] vertexList = new Vertex[10, 10, 10];
+            for (int i = 0; i < 10; ++i) {
+                for (int j = 0; j < 10; ++j) {
+                    for (int k = 0; k < 10; ++k) {
+                        vertexList[i, j, k] = new Vertex(i, j, k);
+                        try {
+                            vertexList[i, j, k].AddContract(vertexList[i - 1, j - 1, k - 1]);
+                            vertexList[i - 1, j - 1, k - 1].AddContract(vertexList[i, j, k]);
+                        } catch (IndexOutOfRangeException) {
+                            ;
+                        }
+                        try {
+                            vertexList[i, j, k].AddContract(vertexList[i, j - 1, k - 1]);
+                            vertexList[i, j - 1, k - 1].AddContract(vertexList[i, j, k]);
+                        }
+                        catch (IndexOutOfRangeException) {
+                            ;
+                        }
+                        try {
+                            vertexList[i, j, k].AddContract(vertexList[i - 1, j, k - 1]);
+                            vertexList[i - 1, j, k - 1].AddContract(vertexList[i, j, k]);
+                        }
+                        catch (IndexOutOfRangeException) {
+                            ;
+                        }
+                        try {
+                            vertexList[i, j, k].AddContract(vertexList[i - 1, j - 1, k]);
+                            vertexList[i - 1, j - 1, k].AddContract(vertexList[i, j, k]);
+                        }
+                        catch (IndexOutOfRangeException) {
+                            ;
+                        }
+                        net.Add(vertexList[i, j, k]);
+                    }
+                }
+            }
+
+            Vertex start = vertexList[0, 0, 0];
+            Vertex end = vertexList[9, 9, 9];
+            Core core = new Core();
+
+            List<Vertex> wayPoints = core.GenerateWayPointSecquence(net, start, end);
+
+            Assert.AreEqual(9, wayPoints.Count);
+            for (int i = 0; i < 9; ++i) {
+                Assert.AreEqual(wayPoints[i], vertexList[i, i, i]);
+            }
+        }
+    }
+}

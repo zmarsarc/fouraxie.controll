eab26835a6fad2c0f824d14a1b7f6eb392fcae5a
zy <zeng.yu.1993@gmail.com>
2017-04-14 14:57:48 +0800

    new class Geometry

---
+++ GroundController/Geometry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace GroundController {
+    class Geometry {
+    }
+
+    class LineStrip {
+
+        private List<Vertex> vertexs;
+
+        public void Extend(List<Vertex> vex) {
+            vertexs.AddRange(vex);
+        }
+
+        public void Add(Vertex vex) {
+            vertexs.Add(vex);
+        }
+
+        public void Draw() {
+
+        }
+    }
+}
